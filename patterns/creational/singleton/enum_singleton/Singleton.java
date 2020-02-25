e716591bca43e25bf022941ebf8f521a173f5cb8
Diukarev <diukarev.sergii@gmail.com>
2020-02-25 11:28:18 +0200

    enum singleton

---
+++ patterns/creational/singleton/enum_singleton/Singleton.java
@@ -0,0 +1,12 @@
+package patterns.creational.singleton.enum_singleton;
+
+
+// + Serialization
+// + Thread safety
+// + Ability to use EnumSet,EnumMap,etc.
+// + Switch support
+// - No lazy initialization
+
+public enum Singleton {
+    INSTANCE;
+}
